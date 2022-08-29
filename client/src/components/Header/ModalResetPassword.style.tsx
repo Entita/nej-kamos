a65928e9065679bbe0aea2046b7002bf4b98e168
Entita <64520809+Entita@users.noreply.github.com>
2022-08-29 12:17:57 +0200

    Delete client directory

--- client/src/components/Header/ModalResetPassword.style.tsx
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-
-export const WrapperStyled = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 6px;
-`;
-
-export const InputStyled = styled.input``;
